1eaa4f7f93228721feda8fb1fc14fe3fe6409526
balqeesHmoud <balqeeshmoud2@gmail.com>
2024-02-12 18:46:19 +0200

    lab14

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
- 
 'use strict';
 const express = require('express');
 const app = express();
@@ -7,12 +6,16 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 const cors = require('cors');
-const port= 3007;
 const axios = require('axios');
-// const port = process.env.PORT;
 const apiKey=process.env.API_KEY;
 const jsonData = require('./Movie Data/data.json')
-// const port = process.env.PORT || 3001;
+const port=3000;
+// const port = process.env.PORT;
+const psqlPort = process.env.PSQLPORT;
+const userName = process.env.USERNAME;
+const sqlPass = process.env.SQLPASS;
+const dataBaseName = process.env.DATABASENAME;
+
 
 const { Client } = require('pg')
 const url = `postgres://balqees:0000@localhost:5432/moveislist`
@@ -32,17 +35,90 @@ app.get('/', homeHandler);
 app.post('/addMovie', addMovieHandler);
 app.get('/getMovies', getMoviesHandler);
 
+app.put('/update/:id', updateMovieCommentsHandler);
+app.delete('/delete/:id', deleteMovieHandler);
+app.get('/get/:id', getMovieHandler);
+
+
+
 //handlers 
+
+
+function updateMovieCommentsHandler(req, res) {
+    let movieId = req.params.id;
+    let { comments } = req.body;
+    let sql = `UPDATE movie_data SET comments = $1 WHERE id = $2;`;
+    let values = [comments, movieId];
+    client.query(sql, values).then(result => {
+        res.send("Successfully updated comments");
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+}
+function deleteMovieHandler(req, res) {
+    let movieId = req.params.id;
+    let sql = `DELETE FROM movie_data WHERE id = $1;`;
+    let values = [movieId];
+    client.query(sql, values).then(result => {
+        res.status(204).send("Successfully deleted");
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+}
+
+
+
+function getMovieHandler(req, res) {
+    let movieId = req.params.id;
+    let sql = `SELECT * FROM movie_data WHERE id = $1;`;
+    let values = [movieId];
+    client.query(sql, values).then(result => {
+        const data = result.rows[0];
+        res.json(data);
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+}
 function homeHandler(req, res) {
     res.send("welcome home")
+}
+
+function addMovieHandler(req, res) {
+    console.log(req.body)
+
+    const { title, releaseDate, posterPath, overview, comments } = req.body // destructuring
+    const sql = `INSERT INTO movie_data (title, releaseDate, posterPath, overview, comments)
+                 VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+    const values = [title, releaseDate, posterPath, overview, comments]
+    client.query(sql, values).then((result) => {
+        console.log(result.rows);
+        res.status(201).json(result.rows);
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+}
+
+function getMoviesHandler(req, res) {
+    const sql = `SELECT * FROM movie_data;`
+    client.query(sql).then((result) => {
+        const data = result.rows
+        res.json(data)
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
 }
 
 
 
 
 //Create a constructor function to ensure your data follow the same format.
-function Movie (id,title,releaseDate,posterPath,overview){
-    this.id=id
+function Movie (ID,title,releaseDate,posterPath,overview){
+    this.ID=ID
     this.title=title
     this.releaseDate=releaseDate
     this.posterPath=posterPath
@@ -60,7 +136,7 @@ function listTrendingMoviesHandler (req,res){
     .then(result=>{
         console.log(result.data.results)
         let movesData = result.data.results.map(ele =>{
-            return new Movie(ele.id, ele.title, ele.release_date, ele.poster_path, ele.overview)
+            return new Movie(ele.ID, ele.title, ele.release_date, ele.poster_path, ele.overview)
 
         })
         res.json(movesData)
@@ -82,7 +158,7 @@ function searchHandler(req, res) {
             console.log(req.query.title);
             if (result.data.results) {
                 let moviesData = result.data.results.map(ele => {
-                    return new Movie(ele.id, ele.title, ele.release_date, ele.poster_path, ele.overview);
+                    return new Movie(ele.ID, ele.title, ele.release_date, ele.poster_path, ele.overview);
                 });
                 res.json(moviesData);
             } else {
@@ -132,42 +208,20 @@ function getFavoriteHandler(req,res){
 
 
 //Create a function to handle the server error (status 500)
-app.get('/error',(req,res)=>res.send(error()))
-app.use(function(err,req,res,text){
-    res.type('text/plain')
-    res.status(500)
-    res.send('internal server error 500')
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+});
 
-})
- 
+//Create a function to handle "page not found error" (status 404)
+
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found');
+});
 
 
-function addMovieHandler(req, res) {
-    console.log(req.body)
 
-    const { tiltle, overview, img } = req.body // destructuring
-    const sql = `INSERT INTO movie_data (tiltle, overview, img)
-                 VALUES ($1, $2, $3) RETURNING *;`;
-    const values = [tiltle, overview, img]
-    client.query(sql, values).then((result) => {
-        console.log(result.rows);
-        res.status(201).json(result.rows);
-    }).catch(error => {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    });
-}
 
-function getMoviesHandler(req, res) {
-    const sql = `SELECT * FROM movie_data;`
-    client.query(sql).then((result) => {
-        const data = result.rows
-        res.json(data)
-    }).catch(error => {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    });
-}
 
 client.connect().then(() => {
     app.listen(port, () => {
